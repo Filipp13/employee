5085191e964f34f3b84819d6a2361377f1b24b15
Filipp Antipov <fantipov@deloitte.ru>
2021-11-08 10:47:03 +0300

    cache fixes

--- src/EmployeeApi/Cache/CachingBehavior.cs
+++ src/EmployeeApi/Cache/CachingBehavior.cs
@@ -3,9 +3,7 @@ using Microsoft.Extensions.Caching.Distributed;
 using Microsoft.Extensions.Logging;
 using Microsoft.Extensions.Options;
 using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
+using System.IO;
 using System.Text.Json;
 using System.Threading;
 using System.Threading.Tasks;
@@ -15,38 +13,49 @@ namespace Employee
     public sealed class CachingBehavior<TRequest, TResponse> : IPipelineBehavior<TRequest, TResponse> where TRequest : ICacheableMediatrQuery
     {
         private readonly IDistributedCache cache;
+
         private readonly ILogger logger;
+
         private readonly CacheSettings settings;
-        public CachingBehavior(IDistributedCache cache, ILogger<TResponse> logger, IOptions<CacheSettings> settings)
+
+        public CachingBehavior(
+            IDistributedCache cache, 
+            ILogger<TResponse> logger, 
+            IOptions<CacheSettings> settings)
         {
             this.cache = cache;
             this.logger = logger;
             this.settings = settings.Value;
         }
+
         public async Task<TResponse> Handle(TRequest request, CancellationToken cancellationToken, RequestHandlerDelegate<TResponse> next)
         {
             TResponse response;
+
             if (request.BypassCache) return await next();
-            async Task<TResponse> GetResponseAndAddToCache()
+
+            var cachedResponse = await cache.GetAsync(request.CacheKey, cancellationToken);
+
+            if (cachedResponse is not null)
             {
-                response = await next();
-                var slidingExpiration = request.SlidingExpiration == null ? TimeSpan.FromHours(settings.SlidingExpiration) : request.SlidingExpiration;
-                var options = new DistributedCacheEntryOptions { SlidingExpiration = slidingExpiration };
-                var serializedData = Encoding.Default.GetBytes(JsonSerializer.Serialize(response));
-                await cache.SetAsync((string)request.CacheKey, serializedData, options, cancellationToken);
-                return response;
-            }
-            var cachedResponse = await cache.GetAsync((string)request.CacheKey, cancellationToken);
-            if (cachedResponse != null)
-            {
-                response = JsonSerializer.Deserialize<TResponse>(Encoding.Default.GetString(cachedResponse));
+                response = await JsonSerializer.DeserializeAsync<TResponse>(new MemoryStream(cachedResponse))
+                    ?? throw new Exception("CachingBehavior DeserializeAsync is null");
+
                 logger.LogInformation($"Fetched from Cache -> '{request.CacheKey}'.");
             }
             else
             {
-                response = await GetResponseAndAddToCache();
+
+                response = await next();
+                var slidingExpiration = request.SlidingExpiration == null ? TimeSpan.FromHours(settings.SlidingExpiration) : request.SlidingExpiration;
+                var options = new DistributedCacheEntryOptions { SlidingExpiration = slidingExpiration };
+                var stream = new MemoryStream();
+                await JsonSerializer.SerializeAsync(stream, response);
+                await cache.SetAsync(request.CacheKey, stream.ToArray(), options, cancellationToken);
+
                 logger.LogInformation($"Added to Cache -> '{request.CacheKey}'.");
             }
+
             return response;
         }
     }
