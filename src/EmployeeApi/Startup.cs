fd3d30965b3ec8deb64745faed7d6d4648185138
Filipp Antipov <fantipov@deloitte.ru>
2021-11-26 14:41:04 +0300

    remove swagger

--- src/EmployeeApi/Startup.cs
+++ src/EmployeeApi/Startup.cs
@@ -9,9 +9,7 @@ using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 using Microsoft.IdentityModel.Tokens;
-using Microsoft.OpenApi.Models;
 using System;
-using System.Collections.Generic;
 using System.IdentityModel.Tokens.Jwt;
 using MediatR;
 using System.Reflection;
@@ -74,40 +72,40 @@ namespace EmployeeApi
             services.AddSingleton<IRolesManagment, RolesManagment>();
             services.AddADManagment(Configuration);
             services.AddControllers();
-            services.AddSwaggerGen(c =>
-            {
-                //c.SwaggerDoc("v1", new Info { Title = "You api title", Version = "v1" });
-                c.AddSecurityDefinition("Bearer", new OpenApiSecurityScheme
-                {
-                    Description = @"JWT Authorization header using the Bearer scheme. \r\n\r\n 
-                      Enter 'Bearer' [space] and then your token in the text input below.
-                      \r\n\r\nExample: 'Bearer 12345abcdef'",
-                    Name = "Authorization",
-                    In = ParameterLocation.Header,
-                    Type = SecuritySchemeType.ApiKey,
-                    Scheme = "Bearer"
-                });
+            //services.AddSwaggerGen(c =>
+            //{
+            //    //c.SwaggerDoc("v1", new Info { Title = "You api title", Version = "v1" });
+            //    c.AddSecurityDefinition("Bearer", new OpenApiSecurityScheme
+            //    {
+            //        Description = @"JWT Authorization header using the Bearer scheme. \r\n\r\n 
+            //          Enter 'Bearer' [space] and then your token in the text input below.
+            //          \r\n\r\nExample: 'Bearer 12345abcdef'",
+            //        Name = "Authorization",
+            //        In = ParameterLocation.Header,
+            //        Type = SecuritySchemeType.ApiKey,
+            //        Scheme = "Bearer"
+            //    });
 
-                c.AddSecurityRequirement(new OpenApiSecurityRequirement()
-                {
-                {
-                    new OpenApiSecurityScheme
-                    {
-                    Reference = new OpenApiReference
-                        {
-                        Type = ReferenceType.SecurityScheme,
-                        Id = "Bearer"
-                        },
-                        Scheme = "oauth2",
-                        Name = "Bearer",
-                        In = ParameterLocation.Header,
+            //    c.AddSecurityRequirement(new OpenApiSecurityRequirement()
+            //    {
+            //    {
+            //        new OpenApiSecurityScheme
+            //        {
+            //        Reference = new OpenApiReference
+            //            {
+            //            Type = ReferenceType.SecurityScheme,
+            //            Id = "Bearer"
+            //            },
+            //            Scheme = "oauth2",
+            //            Name = "Bearer",
+            //            In = ParameterLocation.Header,
 
-                    },
-                    new List<string>()
-                    }
-                });
+            //        },
+            //        new List<string>()
+            //        }
+            //    });
 
-            });
+            //});
         }
 
         public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
@@ -115,16 +113,16 @@ namespace EmployeeApi
             authenticationApi = app.ApplicationServices.GetService<IAuthenticationApi>() 
                 ?? throw new Exception("AuthenticationApi is not resolved");
 
-            app.UseSwagger(c =>
-            {
-                c.RouteTemplate = "api/employee/swagger/{documentname}/swagger.json";
-            });
+            //app.UseSwagger(c =>
+            //{
+            //    c.RouteTemplate = "api/employee/swagger/{documentname}/swagger.json";
+            //});
 
-            app.UseSwaggerUI(c =>
-            {
-                c.SwaggerEndpoint("/api/employee/swagger/v1/swagger.json", "Employee API");
-                c.RoutePrefix = "api/employee/swagger";
-            });
+            //app.UseSwaggerUI(c =>
+            //{
+            //    c.SwaggerEndpoint("/api/employee/swagger/v1/swagger.json", "Employee API");
+            //    c.RoutePrefix = "api/employee/swagger";
+            //});
 
             if (env.IsDevelopment())
             {
