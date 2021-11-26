729334bf1facf2b06f41c42e2158fdb8434f4fa2
Filipp Antipov <fantipov@deloitte.ru>
2021-11-26 15:15:50 +0300

    test

--- src/EmployeeApi/Startup.cs
+++ src/EmployeeApi/Startup.cs
@@ -141,9 +141,9 @@ namespace EmployeeApi
 
             app.UseRouting();
 
-            app.UseAuthentication();
+            //app.UseAuthentication();
 
-            app.UseAuthorization();
+            //app.UseAuthorization();
 
             app.UseEndpoints(endpoints =>
             {
