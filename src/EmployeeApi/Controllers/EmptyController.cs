4b883b8a01aef89a657f8c96144eaba88d2dc30d
Filipp Antipov <fantipov@deloitte.ru>
2022-01-27 12:55:46 +0300

    частично отключен кэш

---
+++ src/EmployeeApi/Controllers/EmptyController.cs
@@ -0,0 +1,24 @@
+﻿using MediatR;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+
+namespace Employee.Api.Controllers
+{
+    [AllowAnonymous]
+    [ApiController]
+    [Route("api/employee")]
+    public class EmptyController : ControllerBase
+    {
+        /// <summary>
+        /// Для теста производительности, возвращает текущую дату
+        /// </summary>
+        /// <returns></returns>
+        [HttpGet("datetime")]
+        public ActionResult GetDatetime()
+        => Ok(DateTime.Now);
+
+    }
+}
